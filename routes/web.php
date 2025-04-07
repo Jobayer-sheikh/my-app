3c87a502ae52ea85a3ac401aee6921fb5c194ec0
jobayer006 <jobayersheikhbd@gmail.com>
2025-04-07 23:02:44 +0600

    Update local nginx config

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/health', function () {
+    return response()->json(['status' => 'ok']);
+});
+
+Route::get('/ip', function () {
+    $serverIp = gethostbyname(gethostname()); // Get the server's IP address
+    return response()->json(['server_ip' => $serverIp]);
+});
